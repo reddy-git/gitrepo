f7c9d56316fb6e7b918b23a72f54d63631d94d2c
hemanth <hemanth1992.june@gmail.com>
2017-08-28 13:45:43 +0530

    chnages done on dev3

--- dev3.sh
+++ dev3.sh
@@ -1,5 +1,5 @@
 
-hello welcome to dev3 and new change
+hello welcome to dev3 and new change........................check rebase once
 
 hello this is dev3 file
 adding content to check recursive merge
